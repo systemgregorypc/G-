c190d993357dc48ececad4716a4e2feb5faedebd
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2020-07-26 21:47:41 -0400

    Create basls.py

---
+++ basls.py
@@ -0,0 +1,21 @@
+from Sys import *
+
+def  open_file (filename):
+     data = open(fileename, "r").read()
+     return  data 
+ Grint(filename)
+ 
+ det G(filecntents):
+ filecontents = lit
+ filecontent =  filecontent
+ for char in  filecontent:
+      if
+    
+def run():}
+  data = open_file(argv [1]) 
+  G (data)
+   
+ run()
+   
+   
+   
